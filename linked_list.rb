3647a3ac867f4070ab02c4cdace07a8e6463d6cf
Lance <dogmandevices@gmail.com>
2024-04-14 12:42:39 -0500

    run tests on most methods; rewrite contains? to check value of node

--- linked_list.rb
+++ linked_list.rb
@@ -46,11 +46,12 @@ class LinkedList
 
   def contains?(value)
     # returns true if the passed in value is in the list and otherwise returns false
-    @list.any?(value)
+    @list.any? { |node| node.val == value }
   end
 
   def find(value)
     # returns the index of the node containing value, or nil if not found
+    @list.find_index(value)
   end
 
   def to_s
@@ -63,6 +64,9 @@ end
 
 # represents each node of the list
 class Node
+  # pos begins at 0, because the first node will be at index 0 in the list.
+  # it points to the index where the next node will be
+  # all Nodes need to know about it, so it is a class variable
   @@pos = 0
   attr_accessor :val, :pos
 
@@ -76,7 +80,7 @@ class Node
   end
 
   def next_node
-    @nxt
+    @@pos
   end
 
   def to_s
@@ -87,6 +91,6 @@ end
 list = LinkedList.new
 node1 = list.append('test')
 node2 = list.append('test2')
-
-puts list
-puts list.size
+node3 = list.prepend('test3')
+puts "list #{list}"
+p list.contains?('test2')
