a60ac353e258e71ea124d64e9d792e1f0bb5f218
Lance <dogmandevices@gmail.com>
2024-04-13 23:22:45 -0500

    temporaily fill in methods with the appropriate array method - need to connect them to Nodes

--- linked_list.rb
+++ linked_list.rb
@@ -1,35 +1,48 @@
 # represents the full list
 class LinkedList
+  attr_accessor :list
+  def initialize
+    @list = []
+  end
+
   def append(value)
     # add new node containing value to the end of the list
+    @list << value
   end
 
   def prepend(value)
     # add new node containing value to the start of the list
+    @list.unshift(value)
   end
 
   def size
     # returns the total number of nodes in the list
+    @list.length
   end
 
   def head
     # returns the first node in the list
+    @list[0]
   end
 
   def tail
     # returns the last node in the list
+    @list[-1]
   end
 
   def at(index)
     # returns the node at the given index
+    @list[index]
   end
 
   def pop
     # removes the last eleent from the list
+    @list.pop
   end
 
   def contains?(value)
     # returns true if the passed in value is in the list and otherwise returns false
+    @list.any?(value)
   end
 
   def find(value)
